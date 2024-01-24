4244d3a974e498ae008f928e39c35e383c3ecbc5
unknown <shubham5027>
2024-01-24 12:25:43 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,41 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]:%(message)s:')
+
+project_name = "CNNClassifier"
+list_of_files =[
+
+    ".github/workflow/.gitkeep",
+    f"src/{project_name}/_init_.py",
+    f"src/{project_name}/components/_init_.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt"
+    "setup.py",
+    "research/trails.ipynb",
+
+
+    ]
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory {filedir}")
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"Creating file {filepath}")
+    else:
+        logging.info(f"{filename} is already exists")
